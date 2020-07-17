8fb5f4a805f53c42926f89d6800f1df7b4994246
Ostlora <66116268+Ostlora@users.noreply.github.com>
2020-07-17 18:32:28 +0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,4 +1,10 @@
 def my_func(name, surname, date, city, email, tel):
+    surname = input("введите фамилию - ")
+    name = input("введите имя - ")
+    date = input("введите дату рождения - ")
+    city = input("введите город проживания - ")
+    email = input("введите email -  ")
+    tel = input("введите телефон - ")
     return '_'.join([name, surname, date, city, email, tel])
 
-print(my_func(surname = 'Аноним', name = 'Илюша', tel = '891111111б', date = '1987', city = 'Париж', email = '404@mail.ru'))
+print(my_func(surname = '', name = '', tel = '', date = '', city = '', email = ''))
