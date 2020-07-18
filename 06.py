474e013b617cee1e29f1678bac33325831a88d69
Ostlora <66116268+Ostlora@users.noreply.github.com>
2020-07-18 12:49:13 +0300

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,12 @@
+def int_func():
+
+    first_letter_small = word[0]
+    first_letter_big = chr(ord(first_letter_small) - ord('a') + ord('A'))
+    return first_letter_big + word[1:]
+
+
+source = input('Введите фразу маленькими латинским буквами - ').split()
+res = []
+for word in source:
+    res.append(int_func())
+print(" ".join(res))
